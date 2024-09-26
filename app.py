a9361c84394a8935f37af48aa17f444a8b040061
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-09-26 19:52:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,22 +93,21 @@ def main():
     # if user_question:
     #     user_input(user_question)
 
-    with st.sidebar:
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                knowledgeBase = get_vector_store(text_chunks)
-                # get_vector_store(text_chunks)
-                st.success("Done")
+    st.title("Menu:")
+    pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+    if st.button("Submit & Process"):
+        with st.spinner("Processing..."):
+            raw_text = get_pdf_text(pdf_docs)
+            text_chunks = get_text_chunks(raw_text)
+            knowledgeBase = get_vector_store(text_chunks)
+            # get_vector_store(text_chunks)
+            st.success("Done")
 
     
-        user_question = st.text_input("Ask a Question from the PDF Files")
+    user_question = st.text_input("Ask a Question from the PDF Files")
 
-        if user_question:
-            user_input(user_question)
+    if user_question:
+        user_input(user_question)
 
 if __name__ == "__main__":
     main()
