26ff3d88fbcf8b23989277a5f919389d748bfae9
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-09-26 17:15:15 +0530

    Update testGoogleHenAI.py

--- testGoogleHenAI.py
+++ testGoogleHenAI.py
@@ -25,7 +25,7 @@ def get_gemini_response(input,image):
 
 st.set_page_config(page_title="Gemini Image Demo")
 with st.sidebar:
-    GOOGLE_API_KEY = st.text_input("Gemini API Key", key="input", type="password")
+    GOOGLE_API_KEY = st.text_input("Gemini API Key", key="input2", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
