4240095c4312aae10b5193c55347c4034034cd4f
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-09-26 11:04:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# load_dotenv()
+# os.getenv("GOOGLE_API_KEY")
+# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+GOOGLE_API_KEY="AIzaSyBzf0bkSuLUmsK1cpFlfzIqRumzbVLpAHk"
+genai.configure(api_key=GOOGLE_API_KEY)
 
 
 
