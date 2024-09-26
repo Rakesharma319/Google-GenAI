cb1a7042163beb98dbdc78c209f5812c8541d1fa
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-09-26 17:28:54 +0530

    Update testGoogleHenAI.py

--- testGoogleHenAI.py
+++ testGoogleHenAI.py
@@ -24,17 +24,14 @@ def get_gemini_response(input,image):
 ##initialize our streamlit app
 
 st.set_page_config(page_title="Gemini Image Demo")
-with st.sidebar:
-    GOOGLE_API_KEY = st.text_input("Gemini API Key", key="GOOGLE_API_KEY", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
+GOOGLE_API_KEY = st.text_input("Gemini API Key", key="GOOGLE_API_KEY", type="password")
+    
 if not GOOGLE_API_KEY:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
+else:
+    genai.configure(api_key="GOOGLE_API_KEY")
     
-genai.configure(api_key="GOOGLE_API_KEY")
 st.header("Image Q&A APP - Gemini Gen AI")
 input=st.text_input("Input Prompt: ",key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
